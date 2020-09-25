7b16deb5dcf391bd7323a9d6d17aaa3cf30efc33
Diego Neri <diego@diegoneri.com>
2020-09-25 20:45:56 -0300

    Finaliza o programa MilhasParaKm

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entradaMilhas;
+            double milhas, quilometros;
+
+            Console.Write("Entre com a medida (em milhas): ");
+            
+            entradaMilhas = Console.ReadLine();           
+            milhas = Convert.ToDouble(entradaMilhas);
+
+            quilometros = milhas * 1.609;
+
+            Console.WriteLine($"{milhas} milha(s) corresponde(m) a {quilometros} quilômetro(s)");
+
+        }
+    }
+}
